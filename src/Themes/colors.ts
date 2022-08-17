e241b59f4bef759f6afa156e841586957413f922
foliaco <foliaco18@gmail.com>
2022-08-17 03:27:10 -0500

    base

---
+++ src/Themes/colors.ts
@@ -0,0 +1,13 @@
+const Light={
+    primary:'#248CF3',
+    secondary:"#F3244A",
+    text:"rgba(10,10,10,0.8)",
+    toggleText:"rgba(255,255,255,0.8)",
+    sombra:"rgba(10,10,10,0.8)",
+    togleSombra:"rgba(255,255,255,0.8)",
+    fondo:"rgba(245,245,245,0.8)"
+}
+
+const Theme={Light};
+
+export default Theme;
---
+++ src/modules/Reports/NabBarReport.tsx
@@ -0,0 +1,7 @@
+export  const NavBarReport=()=>{
+    return (
+        <header>
+
+        </header>
+    )
+}
